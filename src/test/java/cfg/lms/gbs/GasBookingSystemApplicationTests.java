bb52d03fd0cdcda82317454acd229ce69ceab737
SriChaitanyaGit <gnanasrichaitanya2004@gmail.com>
2025-07-17 14:07:23 +0530

    initial setup

---
+++ src/test/java/cfg/lms/gbs/GasBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cfg.lms.gbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
