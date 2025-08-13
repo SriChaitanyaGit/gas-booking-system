af3eeee2975332d9c891d708e16275786acb70df
SriChaitanyaGit <gnanasrichaitanya2004@gmail.com>
2025-08-13 14:23:59 +0530

    Add files via upload

---
+++ src/main/java/cfg/lms/gbs/dtos/GasAgencyDTO.java
@@ -0,0 +1,12 @@
+package cfg.lms.gbs.dtos;
+
+import lombok.Data;
+
+@Data
+public class GasAgencyDTO {
+    private int gasid;
+    private String name;
+    private String location;
+    private int domesticCylinders;
+    private int commercialCylinders;
+}
